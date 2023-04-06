30fc596c0e487246a1bf1c37afe688926db196bb
TobyVB <98196156+TobyVB@users.noreply.github.com>
2023-04-06 14:16:23 +0800

    use loader data so images in projects page load before page is rendered

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,52 @@ import Resume from "./components/Resume";
 // Layouts
 import SharedLayout from "./layouts/SharedLayout";
 
+const mainLoader = async () => {
+  const projectData = [
+    {
+      path: "https://user-images.githubusercontent.com/98196156/221370173-e6f67b4e-ae06-41b4-8b98-82f5f365e61e.png",
+      size: "100%",
+      title: "The TalkSpace",
+      info: "This is an ongoing project I am working on. It is a social media site inspired by Reddit, Facebook, Myspace, Quora and Github",
+      github: "https://github.com/TobyVB/Talkspace",
+      demo: "https://fir-practice-cace4.web.app/",
+    },
+    {
+      path: "https://user-images.githubusercontent.com/98196156/221128205-2bbbedc7-b11a-4072-9a97-baa6f2c300cc.jpg",
+      size: "40%",
+      title: "SuperChat",
+      info: "SuperChat is a chatroom web app that utilizes google authentication. I completed this project as a code-along by Fireship",
+      github: "https://github.com/TobyVB/SuperChat",
+      demo: "https://superchat-aa35f.web.app/",
+    },
+    {
+      path: "https://user-images.githubusercontent.com/98196156/221082488-c0eec32b-a125-489c-9857-64e5014e9118.jpg",
+      size: "40%",
+      title: "Quiz App",
+      info: "This Quiz web app uses an API provided from the Open Trivia website. Users can filter questions by count, type, category, and difficulty. Users are then scored by number of correct selected answers.",
+      github: "https://github.com/TobyVB/Quiz-App",
+      demo: "https://quiz-app-29f85.web.app/",
+    },
+    {
+      path: "https://user-images.githubusercontent.com/98196156/226163163-48309ea3-a83e-45c9-983d-8087bc28e2b0.png",
+      size: "100%",
+      title: "Movie DB App",
+      info: "This movie database web app uses an API provided from the The Movie Database website.",
+      github: "https://github.com/TobyVB/movieDB",
+      demo: "https://tobyvb-movie-db.vercel.app/",
+    },
+    {
+      path: "https://user-images.githubusercontent.com/98196156/226164053-2a55833c-6c09-42c9-9514-e9d665980a7f.png",
+      size: "100%",
+      title: "Blog App",
+      info: "This Blog web app was made by following the NextJS documentation",
+      github: "https://github.com/TobyVB/Blog",
+      demo: "https://tobyvbsblog.vercel.app/",
+    },
+  ];
+  return projectData;
+};
+
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route
@@ -22,7 +68,7 @@ const router = createBrowserRouter(
     >
       <Route index element={<Homepage />} />
       <Route path="about" element={<About />} />
-      <Route path="projects" element={<Projects />} />
+      <Route path="projects" element={<Projects />} loader={mainLoader} />
       <Route path="resume" element={<Resume />} />
     </Route>
   )
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,3 +1,4 @@
+import { useLoaderData } from "react-router-dom";
 import { useEffect, useState } from "react";
 export default function Projects() {
   useEffect(() => {
@@ -5,84 +6,12 @@ export default function Projects() {
   }, []);
 
   const [open, setOpen] = useState(false);
-
-  const projectData = [
-    {
-      path: "https://user-images.githubusercontent.com/98196156/221370173-e6f67b4e-ae06-41b4-8b98-82f5f365e61e.png",
-      size: "100%",
-      title: "The TalkSpace",
-      info: "This is an ongoing project I am working on. It is a social media site inspired by Reddit, Facebook, Myspace, Quora and Github",
-      github: "https://github.com/TobyVB/Talkspace",
-      demo: "https://fir-practice-cace4.web.app/",
-    },
-    {
-      path: "https://user-images.githubusercontent.com/98196156/221128205-2bbbedc7-b11a-4072-9a97-baa6f2c300cc.jpg",
-      size: "40%",
-      title: "SuperChat",
-      info: "SuperChat is a chatroom web app that utilizes google authentication. I completed this project as a code-along by Fireship",
-      github: "https://github.com/TobyVB/SuperChat",
-      demo: "https://superchat-aa35f.web.app/",
-    },
-    {
-      path: "https://user-images.githubusercontent.com/98196156/221082488-c0eec32b-a125-489c-9857-64e5014e9118.jpg",
-      size: "40%",
-      title: "Quiz App",
-      info: "This Quiz web app uses an API provided from the Open Trivia website. Users can filter questions by count, type, category, and difficulty. Users are then scored by number of correct selected answers.",
-      github: "https://github.com/TobyVB/Quiz-App",
-      demo: "https://quiz-app-29f85.web.app/",
-    },
-    {
-      path: "https://user-images.githubusercontent.com/98196156/226163163-48309ea3-a83e-45c9-983d-8087bc28e2b0.png",
-      size: "100%",
-      title: "Movie DB App",
-      info: "This movie database web app uses an API provided from the The Movie Database website.",
-      github: "https://github.com/TobyVB/movieDB",
-      demo: "https://tobyvb-movie-db.vercel.app/",
-    },
-    {
-      path: "https://user-images.githubusercontent.com/98196156/226164053-2a55833c-6c09-42c9-9514-e9d665980a7f.png",
-      size: "100%",
-      title: "Blog App",
-      info: "This Blog web app was made by following the NextJS documentation",
-      github: "https://github.com/TobyVB/Blog",
-      demo: "https://tobyvbsblog.vercel.app/",
-    },
-  ];
+  const projectData = useLoaderData();
 
   function ProjectsArray() {
     return projectData.map((project, index) => {
       return (
         <div key={index}>
-          {/* INFO */}
-          {/* <div
-            style={{
-              width: "100%",
-              display: `${open !== project.title ? "none" : ""}`,
-            }}
-          >
-            <div
-              onClick={() => setOpen(false)}
-              style={{
-                // position: "fixed",
-                width: "350px",
-                padding: "1em",
-                borderRadius: "3px",
-                left: "0",
-                right: "0",
-                top: "50%",
-                margin: "auto",
-                background: "black",
-                zIndex: "1",
-                background: "rgba(0,0,0,.2)",
-                color: "white",
-                border: "1px solid white",
-              }}
-            >
-              {project.info}
-            </div>
-          </div> */}
-          {/* END-INFO */}
-
           <h3>{project.title}</h3>
 
           <div
